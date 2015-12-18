105c4c00652010ead01da3dce398d93c9f3b37de
Joseph Shearer <joseph@josephshearer.net>
2015-12-17 22:36:20 -0500

    Added new workplace

--- app/components/Home/Resume.jsx
+++ app/components/Home/Resume.jsx
@@ -8,36 +8,40 @@ import '../../semantic/dist/components/header.css';
 import '../../semantic/dist/components/progress.css';
 import '../../semantic/dist/components/progress.js';
 
-class SubjectContainer extends React.Component{
-	render(){
-		return (
-			<div className={styles.subject_container}>
+class SubjectContainer extends React.Component {
+    render() {
+        return (
+            <div className={styles.subject_container}>
 				<h2 className={styles['subject-title']}>{this.props.label}</h2>
 				{this.props.children}
 			</div>
-		)
-	}
+        )
+    }
 }
 SubjectContainer.propTypes = {
-	label: React.PropTypes.string,
-	children: React.PropTypes.arrayOf(ComponentContainer)
+    label: React.PropTypes.string,
+    children: React.PropTypes.arrayOf(ComponentContainer)
 }
 
 //Enforce that SubjectContainer's children must be ComponentContainers
 
-class ComponentContainer extends React.Component{
-	render(){
-		let skills = _.map(this.props.skills, (skill) => {
-			if(skill.bar){
-				return <SkillBar {...skill}/>
-			} else{
-				return <Skill {...skill}/>;
-			}
-		})
-		let right_info = ((skills) => {
-		          		if(this.props.location||this.props.time||this.props.website){
-		          			return (
-			          					<div>
+class ComponentContainer extends React.Component {
+    render() {
+        let skills = _.map(this.props.skills, (skill) => {
+            if (skill.bar) {
+                return <SkillBar {...skill
+                }
+                />
+            } else {
+                return <Skill {...skill
+                }
+                />;
+            }
+        })
+        let right_info = ((skills) => {
+            if (this.props.location || this.props.time || this.props.website) {
+                return (
+                    <div>
 			          						<strong>{this.props.location}</strong><br />
 			            					<em>{this.props.time}</em><br />
 			            					<a href={this.props.website}>{this.props.website}</a>
@@ -53,13 +57,13 @@ class ComponentContainer extends React.Component{
 			            						}
 			            					})(skills)}
 			            				</div>
-		            				)
-		          		} else {
-		          			return this.props.right_info;
-		          		}
-		          	})(skills);
-		return (
-			<div className={styles["component-container"]}>
+                )
+            } else {
+                return this.props.right_info;
+            }
+        })(skills);
+        return (
+            <div className={styles["component-container"]}>
 				<h3 className={styles.title}>{this.props.title}</h3>
 
 		        <div className={styles["info-wrapper"]+" ui grid"}>
@@ -72,35 +76,71 @@ class ComponentContainer extends React.Component{
 		          </div>
 		        </div>
 			</div>
-		)
-	}
+        )
+    }
 }
 
-class SkillBar extends React.Component{
-	render(){
-		return (
-				<div className={"ui tiny "+this.props.color+" progress"} data-percent={this.props.pct}>
+class SkillBar extends React.Component {
+    render() {
+        return (
+            <div className={"ui tiny "+this.props.color+" progress"} data-percent={this.props.pct}>
 				  <div className="bar" style={{"transition-duration": "300ms", "width": this.props.pct+"%"}}></div>
 				  <div className="label">{this.props.name}</div>
 				</div>
-			);
-	}
+        );
+    }
 }
 
-class Skill extends React.Component{
-	render(){
-		return (
-				<div className={"ui medium "+this.props.color+" label "+styles['label-fix']} data-percent={this.props.pct}>{this.props.name}</div>
-			);
-	}
+class Skill extends React.Component {
+    render() {
+        return (
+            <div className={"ui medium "+this.props.color+" label "+styles['label-fix']} data-percent={this.props.pct}>{this.props.name}</div>
+        );
+    }
 }
 
 
 export class Resume extends React.Component {
-	render() {
-		return (
-			<div className={styles.body}>
+    render() {
+        return (
+            <div className={styles.body}>
 				<SubjectContainer label="Work Experience">
+					<ComponentContainer title="iCitizen - Data Science"
+										location="Rochester, NY"
+										time="August - Present, 2015"
+										website="www.icitizen.com"
+										skills={[
+											{"name":"Javascript","color":"brown"},
+											{"name":"ES6","color":"green"},
+											{"name":"Webpack","color":"red"},
+											{"name":"React", "color":"blue"},
+											{"name":"Flux - Redux", "color":"blue"},
+											{"name": "Immutable(.js)", "color":"blue"},
+											{"name":"Leaflet", "color":"green"},
+											{"name":"Mapbox", "color":"green"},
+											{"name":"GIS", "color":"orange"},
+											{"name":"GeoJSON", "color":"orange"},
+											{"name":"MongoDB", "color":"purple"},
+											{"name":"Microservices", "color":"purple"},
+											{"name":"Python", "color":"tan"},
+											{"name":"Data transformation", "color":"tan"},
+											{"name":"D3","color":"teal"},
+											{"name":"LESS/SASS","color":"red"},
+											{"name":"Semantic UI","color":"red"},
+										]}>
+						<p className={styles.p}>
+							Using Python, I transformed various forms of imput data (I.E voter records in CSV, TSV, XML formats) to a standardized layout in JSON, and then stored the resulting JSON object in MongoDB for later use.
+						</p>
+						<p className={styles.p}>
+							Using modern Javascript (ES6, JS2015, ES7, etc. all through Babel) and tools (NPM, grunt, etc), I am building an interactive map-based visualization.
+						</p>
+						<ul className={styles.ul}>
+							<li className={styles.li}>This visualization will handle being in numerous different states with different expected responses, at different zoom levels.</li>
+							<li className={styles.li}>It quickly loads and displays many GeoJSON shapes with (up to) tens of thousands of vertices each.</li>
+							<li className={styles.li}>It quickly loads and displays (efficently, using clustering where appropriate) tens of thousands of interactive markers, which allow interaction with other aspects of the UI.</li>
+							<li className={styles.li}>It handles state transitions (using Flux, Redux) in such a way as to make them fully reversible and replayable. Moving up or <i>"back"</i> is trivial.</li>
+						</ul>
+					</ComponentContainer>
 					<ComponentContainer title="CeASAR - The Center for Adolescent Substance Abuse Research @ Harvard Medical School" 
 										location="Boston, MA" 
 										time="June - August 2015"
@@ -320,6 +360,6 @@ export class Resume extends React.Component {
 					</ComponentContainer>
 				</SubjectContainer>
 			</div>
-		);
-	}
+        );
+    }
 }
